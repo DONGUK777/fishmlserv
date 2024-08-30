e5048593985ee06cf6e4339cdd43920b9099bde3
DONGUK777 <kimdongyork@gmail.com>
2024-08-30 12:05:12 +0900

    edit main.py

--- src/fishmlserv/main.py
+++ src/fishmlserv/main.py
@@ -15,6 +15,16 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 @app.get("/fish")
-def fish(length, weight):
+def fish(length: float, weight: float)
+    """
+    물고기의 종류 판별기
+
+    Args:
+        length (float): 물고기 길이(cm)
+        weight (float): 물고기의 무게(g)
+    Returns:
+        dict: 물고기 종류를 담은 딕셔너리
+    """
+
     return {"length": length, "weight": weight}
 
