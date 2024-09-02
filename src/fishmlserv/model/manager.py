a1907ed96a0b33185be246021d5fcb0f8307c65c
DONGUK777 <kimdongyork@gmail.com>
2024-09-02 14:17:06 +0900

    some edit

--- src/fishmlserv/model/manager.py
+++ src/fishmlserv/model/manager.py
@@ -1,5 +1,13 @@
+import os
 
 def get_model_path():
+    my_path = __file__
+    dir_name = os.path.dirname(my_path)
+    model_path = os.path.join(dir_name, "model.pkl")
+    
+    return model_path
+
+#def get_model_path():
     # import os ...
     # 이 함수 파일의 절대 경로를 받아온다
     # 절대 경로를 이용해 model.pkl의 경로를 조합
@@ -7,4 +15,4 @@ def get_model_path():
 
     # 사용 fastapi main.py 에서 아래와 같이 사용
     # from fishmlserv.model.manager import get_model_path
-    return "living for today"
+#    return "living for today"
