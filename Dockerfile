31430b4f0576aa46e78e8935c053d8130e242719
DONGUK777 <kimdongyork@gmail.com>
2024-09-03 14:49:18 +0900

    all edit

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,7 @@ FROM datamario24/python311scikitlearn-fastapi:1.0.0
 
 WORKDIR /code
 
-COPY src /code/src
-COPY pyproject.toml /code/
+COPY src/fishmlserv/main.py /code/
 
 RUN pip install --no-cache-dir --upgrade  git+https://github.com/DONGUK777/fishmlserv.git@0.8/hub
 
--- src/fishmlserv/model/manager.py
+++ src/fishmlserv/model/manager.py
@@ -1,5 +1,5 @@
 import os
-import argparse
+import pickle
 
 def get_model_path():
     my_path = __file__
@@ -8,20 +8,33 @@ def get_model_path():
     
     return model_path
 
-def main():
-    parser = argparse.ArgumentParser(description="Get the model path.")
-    args = parser.parse_args()
-    print(f"'{get_model_path()}'")
 
-if __name__ == "__main__":
-    main()
+def run_prediction(l: float, w: float):
+    """
+    주어진 물고기의 길이(l)와 무게(w)를 기반으로 해당 물고기의 종류를 예측하는 함수.
 
-#def get_model_path():
-    # import os ...
-    # 이 함수 파일의 절대 경로를 받아온다
-    # 절대 경로를 이용해 model.pkl의 경로를 조합
-    # 조합된 경로를 리턴 = 끝
+    Args:
+        l (float): 물고기의 길이.
+        w (float): 물고기의 무게.
 
-    # 사용 fastapi main.py 에서 아래와 같이 사용
-    # from fishmlserv.model.manager import get_model_path
-#    return "living for today"
+    Returns:
+        str: 예측된 물고기의 종류로, '도미' 또는 '빙어' 중 하나를 반환.
+
+    동작:
+    1. 사전 학습된 모델의 경로를 가져온다.
+    2. 해당 경로에서 모델을 불러온다.
+    3. 불러온 모델을 사용하여 주어진 길이와 무게에 대해 예측을 수행한다.
+    4. 예측 결과에 따라 '도미' 또는 '빙어'를 반환한다.
+    """
+    model_path = get_model_path()
+    with open(model_path, "rb") as f:
+        fish_model = pickle.load(f)
+
+    p = fish_model.predict([[l, w]])
+
+    if p[0] == 1:
+        fish_class = "도미"
+    else:
+        fish_class = "빙어"
+
+    return fish_class
